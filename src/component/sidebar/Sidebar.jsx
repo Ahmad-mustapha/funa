8846576145bec1de849a365560ec5d03df08f47b
Ahmad-mustapha <ahmadishola12@gmail.com>
2025-01-10 01:01:42 +0100

    fixes

--- src/component/sidebar/Sidebar.jsx
+++ src/component/sidebar/Sidebar.jsx
@@ -2,30 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Profileimage2 from '../../assets/profileimg2.png'
 import { MdOutlineDashboard } from "react-icons/md";
+import { TbTransferIn } from "react-icons/tb";
+import { CiUser } from "react-icons/ci";
+import { GoPeople } from "react-icons/go";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BiLogOut } from "react-icons/bi";
+import { TbTransform } from "react-icons/tb";
 const navLinks = [
-  {id: 1, text: 'Dashboard', icon: <MdOutlineDashboard />, link: '/tennis'},
-  {id: 2, text: 'Transaction', icon: <MdOutlineDashboard />, link: '/padel'},
-  {id: 3, text: 'Users', icon: <MdOutlineDashboard />, link: '/pickleball'},
-  {id: 4, text: 'KYC', icon: <MdOutlineDashboard />, link: '/association'},
-  {id: 5, text: 'Payout', icon: <MdOutlineDashboard />, link: '/centers'},
-  {id: 6, text: 'Team', icon: <MdOutlineDashboard />, link: '/teachers'},
-  {id: 7, text: 'Setting', icon: <MdOutlineDashboard />, link: '/teachers'},
-  {id: 8, text: 'Log Out', icon: <MdOutlineDashboard />, link: '/teachers'},
+  {id: 1, text: 'Dashboard', icon: <MdOutlineDashboard />, link: '/'},
+  {id: 2, text: 'Transaction', icon: <TbTransform />, link: '/transaction'},
+  {id: 3, text: 'Users', icon: <CiUser />, link: '/users'},
+  {id: 4, text: 'KYC', icon: <CiUser />, link: '/kyc'},
+  {id: 5, text: 'Payout', icon: <TbTransferIn />, link: '/payout'},
+  {id: 6, text: 'Team', icon: <GoPeople />, link: '/team'},
+  {id: 7, text: 'Setting', icon: <IoSettingsOutline />, link: '/settings'},
+  {id: 8, text: 'Log Out', icon: <BiLogOut />, link: '/logout'},
 ]
 
 const Sidebar = () => {
   return (
-    <div className='flex items-start flex-col px-6 py-4'>
-      <div className='flex items-center gap-4'>
+    <div className='flex items-start flex-col px-6 py-[.8rem]'>
+      <div className='flex items-center gap-2'>
         <div><img src={Profileimage2} alt="" /></div>
         <p className='font-[700] text-[1.3rem]'>Sanni Ahmed</p>
       </div>
-      <ul className='w-full flex flex-col gap-4 mt-4 bg-green-300'>
+      <ul className='w-full flex flex-col gap-[.8rem] mt-4'>
         {navLinks.map((link, index) =>(
           <li 
           key={index}
-          className='flex items-center gap-2 text-gray-500 w-full text-[1.1rem] bg-red-200 p-1'
-          ><span>{link.icon}</span><Link className='w-full'>{link.text}</Link></li>
+          className='flex items-center gap-2 text-[#2C8CFB] w-full text-[1rem] rounded-xl bg-[#F1F8FF] py-[.4rem] px-3'
+          ><span>{link.icon}</span><Link to={link.link} className='w-full'>{link.text}</Link></li>
         ))}
       </ul>
     </div>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+const summaryData = [
+  {id: 1, title: 'Total Balance', amount: 'N200,000'},
+  {id: 2, title: 'Active Users', amount: 'N100,000'},
+  {id: 3, title: 'Daily Transaction', amount: 'N200,000'}
+]
+
+export const Summary = () =>(
+  <ul className='flex items-center gap-6'>
+    {summaryData.map((data, index) =>(
+      <li className='w-[15.5rem] flex flex-col items-center justify-center gap-2 p-6 px-10 rounded-xl border-[1px] border-gray-200'>
+        <span className='font-[400] text-[14px]'>{data.title}</span><span className='font-[600] text-[30px]'>{data.amount}</span>
+      </li>
+    ))}
+  </ul>
+)
+
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <section className='p-8 rounded-lg bg-white'>
+        <p className='text-[1.3rem] font-[600] mb-4'>User Summary</p>
+        <Summary />
+      </section>
+    </div>
   )
 }
 
