6e4137f14ebefdcf07da2a23585069942418efc9
Ahmad-mustapha <ahmadishola12@gmail.com>
2025-01-12 16:22:25 +0100

    fixes

--- src/pages/transaction/Transaction.jsx
+++ src/pages/transaction/Transaction.jsx
@@ -1,43 +1,51 @@
 import React from 'react'
-import Trans from '../../assets/transactionIcon.png'
-import Cancel from '../../assets/cancelIcon.png'
-import Checkfill from '../../assets/checkfillIcon.png'
-
-const eachTransData = [
-  { id: 1, title: 'Total Subscription', num: 207, image: Trans },
-  { id: 1, title: 'Approved', num: 207, image: Checkfill },
-  { id: 1, title: 'Rejected', num: 207, image: Cancel }
-]
-
-const EachTrans = () =>(
-  <div className='flex items-center gap-6 justify-center flex-wrap xl:justify-start xl:flex-nowrap'>
-    { eachTransData.map((data, index) =>(
-      <div key={index} className='w-[13rem] flex items-center gap-4 bg-white p-4 rounded-xl'>
-        <div className='p-2 rounded-xl'
-          style={{
-            backgroundColor:
-              data.title === "Total Subscription"
-                ? "rgba(56, 101, 215, 0.16)"
-                : data.title === "Approved"
-                ? "rgba(224, 254, 233)"
-                : data.title === "Rejected"
-                && "rgba(231, 77, 60, 0.16)"
-          }}
-        ><img src={data.image} alt="" /></div>
-        <div className='flex flex-col gap-1'>
-          <p className='text-[.85rem] text-[#64748B]'>{data.title}</p>
-          <p className='text-[1.1rem] font-[700]'>{data.num}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-)
+import { MdOutlineFileDownload } from "react-icons/md";
+import { FiFilter } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi";
+import { IoFilterSharp } from "react-icons/io5";
+import { BsEye } from "react-icons/bs";
+import './transaction.css'
 
 const Transaction = () => {
   return (
-    <div>
-      <EachTrans />
-    </div>
+    <>
+      <section className='mt-6 bg-white rounded-xl p-4'>
+        <div className='flex items-center justify-between'>
+          <p className='text-[1.3rem] font-[600] mb-4'>User Summary</p>
+          <div className='flex items-center gap-3'>
+            <div className='relative w-[150px]'><input 
+            placeholder='Search'
+            type="text" 
+            className='w-full border-[1px] border-slate-300 rounded-md p-[.3rem] pl-8'/>
+            <FiSearch className='absolute top-2 left-2 text-[1.1rem] font-[600]'/>
+            </div>
+            <div className='relative w-[100px]'>
+              <select name="" id="" className='w-full relative border-[1px] border-slate-300 rounded-md p-[.3rem]'>
+                <option value="">Monthly</option>
+                <option value=""></option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <table className='w-full mt-6'>
+            <tr className='flex items-center justify-between bg-[#F9FBFC] rounded-[100px] px-6 border-0'>
+              <th>Customer</th>
+              <th>Date</th>
+              <th>Document Type</th>
+              <th>Action</th>
+              <th>View</th>
+            </tr>
+            <tr className='row flex items-center justify-between px-6'>
+              <td>Customer</td>
+              <td>Date</td>
+              <td>Document Type</td>
+              <td>Action</td>
+              <td>View</td>
+            </tr>
+        </table>
+      </section>
+      
+    </>
   )
 }
 
