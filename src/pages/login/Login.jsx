1f79adcd764cfd8e2aeb2e50ec1a146bfb173eab
Ahmad-mustapha <ahmadishola12@gmail.com>
2025-01-30 23:55:00 +0100

    fixes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../assets/funalogo.png'
 import { Link } from 'react-router-dom'
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
+
+
+
 const Login = () => {
+  const [ showPassword, setShowPassword ] = useState(true)
+
+  const handlePassword = () =>{
+    if (type === 'password') {
+      setShowPassword(!showPassword)
+    }
+  }
   return (
     <div className='flex items-center justify-center flex-col h-screen bg-[#F9FBFC]'>
       <div className='flex flex-col items-center justify-center gap-4'>
@@ -16,11 +28,19 @@ const Login = () => {
             className='border-[1px] rounded-lg p-1 w-full'
             type="email" />
           </div>
-          <div>
+          <div className='relative'>
             <label  htmlFor="email" className="text-[14px] font-[500]">Password</label> <br />
             <input 
             className='border-[1px] rounded-lg p-1 w-full'
             type="password" />
+            {
+            showPassword ? <FaRegEye
+            onClick={handlePassword}
+            className='absolute top-8 right-2'/> : 
+            <FaRegEyeSlash
+            onClick={handlePassword}
+            className='absolute top-8 right-2'/>
+            }
           </div>
           <div className="text-right text-red-500 text-[.7rem] underline font-bold"><Link>Forget password</Link></div>
         </div>
